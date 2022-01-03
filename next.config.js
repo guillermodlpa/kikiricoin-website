b83538a48c07b7cb0a7c0e6133f00c0646c17a43
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-01-03 20:26:57 +0100

    🔗 Override webpack config to use local metamask-react that I'm playing with, for https://github.com/VGLoic/metamask-react/issues/13

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // This config ensures we load React from this project, not the libraries. Avoid duplicate React error
+      react: path.join(__dirname, 'node_modules/react'),
+      // Testing the metamask-react project locally. Can't manage with `npm link`
+      'metamask-react': path.join(__dirname, '../guillermodlpa/metamask-react'),
+    };
+    return config;
   },
 };
