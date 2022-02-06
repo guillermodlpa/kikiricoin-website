1a7e10fc19d071a0fabccf10f09eaba32604c9a8
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-02-06 14:07:19 +0100

    🐓 Add rooster emoji attribution in the footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -8,10 +8,17 @@ const Footer = () => {
   return (
     <Box as="footer" px={8} py={8}>
       <Center>
-        <UnorderedList listStyleType="none">
+        <UnorderedList listStyleType="none" spacing={4} textAlign={'center'}>
           <ListItem>
             <DecoratedLink href="https://guillermodelapuente.com" isExternal>
               {t('authorWebsiteLink')}
+            </DecoratedLink>
+          </ListItem>
+
+          <ListItem>
+            The KikiriCoin logo is the{' '}
+            <DecoratedLink href="https://emojipedia.org/google/android-9.0/rooster/" isExternal>
+              Rooster emoji on Google Android 9.0
             </DecoratedLink>
           </ListItem>
         </UnorderedList>
